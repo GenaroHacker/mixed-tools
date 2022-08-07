8c6b5b85aa74d6c0570c9a301b2a309d22d6cde3
GenaroHacker <genarominetto@hotmail.com>
2022-08-07 02:37:26 +0200

    add hello world to go module

--- file creator/create_go.go
+++ file creator/create_go.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -14,4 +15,20 @@ func main() {
 	//Create a folder named test with the file main.py in it
 	os.Mkdir("test go", 0777)
 	os.Create("test go/main.go")
+	//Write a go hello world program to the file main.go
+	f, err := os.OpenFile("test go/main.go", os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	f.WriteString(`// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}`)
 }
