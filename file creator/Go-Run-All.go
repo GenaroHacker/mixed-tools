20d79dd1cd347a65539c0874dfdc00f41ca0b781
GenaroHacker <genarominetto@hotmail.com>
2022-08-08 12:56:33 +0200

    Git-Run-All creates a .bat file to run the program for each go file in the directory

---
+++ file creator/Go-Run-All.go
@@ -0,0 +1,28 @@
+// This file reads all the names of the files with the .go extension in the current directory.
+// It then creates a .bat file for each one that runs the go run command on the file.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	files, err := filepath.Glob("*.go")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for _, file := range files {
+		fmt.Println(file)
+		f, err := os.Create(file + ".bat")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		f.WriteString("go run " + file)
+	}
+}
